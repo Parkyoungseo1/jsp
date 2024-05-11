98a004668d40b7d673ed1e3abbad1808b9f77142
Breadtj <taking99@naver.com>
2024-05-11 15:43:51 +0900

    20240511_1550

--- src/main/java/study2/mapping/Test5Controller.java
+++ src/main/java/study2/mapping/Test5Controller.java
@@ -10,60 +10,60 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
-@WebServlet("*.do4")
-public class Test4Controller extends HttpServlet {
+@WebServlet("*.do5")
+public class Test5Controller extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Test4Interface command = null;
+		Test5Interface command = null;
 		String viewPage = "/WEB-INF/study2/mapping/";
 		
 		String uri = request.getRequestURI();
 		
 		String com = uri.substring(uri.lastIndexOf("/")+1, uri.lastIndexOf("."));
 		
-		if(com.equals("test4")) {
-			viewPage += "test4.jsp";
+		if(com.equals("test5")) {
+			viewPage += "test5.jsp";
 		}
-		else if(com.equals("list")) {
-			command = new Test4ListCommand();
+		else if(com.equals("HeowonList")) {
+			command = new Test5HeowonListCommand();
 			command.execute(request, response);
-			// viewPage = "/WEB-INF/study2/mapping/list.jsp";
-			viewPage += "list.jsp";
+			viewPage += "HeowonList.jsp";
 		}
-		else if(com.equals("input")) {
-			// viewPage = "/WEB-INF/study2/mapping/input.jsp";
-			viewPage += "input.jsp";
+		else if(com.equals("HeowonLogin")) {
+			viewPage += "HeowonLogin.jsp";
 		}
-		else if(com.equals("inputOk")) {
-			command = new Test4InputOkCommand();
+		else if(com.equals("HeowonLoginOk")) {
+			command = new Test5HeowonLoginOkCommand();
 			command.execute(request, response);
 			viewPage = "/include/message.jsp";
 		}
-		else if(com.equals("update")) {
-			command = new Test4UpdateCommand();
+		else if(com.equals("HeowonUpdate")) {
+			command = new Test5UpdateCommand();
 			command.execute(request, response);
-			viewPage += "update.jsp";
+			viewPage += "HeowonUpdate.jsp";
 		}
-		else if(com.equals("updateOk")) {
-			command = new Test4UpdateOkCommand();
-			command.execute(request, response);
-			viewPage = "/include/message.jsp";
-		}
-		else if(com.equals("delete")) {
-			command = new Test4DeleteCommand();
-			command.execute(request, response);
-			viewPage += "delete.jsp";
-		}
-		else if(com.equals("deleteOk")) {
-			command = new Test4DeleteOkCommand();
+		else if(com.equals("HeowonUpdateOk")) {
+			command = new Test5HeowonOkUpdateCommand();
 			command.execute(request, response);
 			viewPage = "/include/message.jsp";
 		}
-		else if(com.equals("search")) {
-			command = new Test4SearchCommand();
+		else if(com.equals("HeowonDelete")) {
+			command = new Test5HeowonDeleteCommand();
 			command.execute(request, response);
-			viewPage += "search.jsp";
+			viewPage += "HeowonDelete.jsp";
 		}
+		else if(com.equals("HeowonDeleteOk")) {
+			command = new Test5HeowonDeleteOkCommand();
+			command.execute(request, response);
+			viewPage = "/include/message.jsp";
+		}
+		else if(com.equals("HeowonSearch")) {
+			command = new Test5HeowonSearchCommand();
+			command.execute(request, response);
+			viewPage += "HeowonSearch.jsp";
+		}
+		
+		
 		
 		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
 		dispatcher.forward(request, response);
